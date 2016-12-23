f61d4158af6bd904ab8aaf3d5c4e323df8b1d4c9
Michael Laszlo <git@michaellaszlo.com>
2016-12-22 23:59:48 -0500

    Enable indefinite-payment checkbox

--- js/paypal_subscription_builder.js
+++ js/paypal_subscription_builder.js
@@ -1,7 +1,15 @@
 var PayPalSubscriptionBuilder = (function () {
 
+  function toggleIndefinitePayments() {
+    console.log('toggling');
+  }
+
   function load() {
+    var button;
     console.log('loading');
+    // Enable indefinite-payment selector.
+    button = document.getElementById('paymentRecursIndefinitely');
+    button.onclick = toggleIndefinitePayments;
   }
 
   return {
