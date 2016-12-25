e88695e528ae9e8bb5483504267eb2e41a61985c
Michael Laszlo <git@michaellaszlo.com>
2016-12-25 02:37:13 -0500

    Implement numerical updates

--- js/paypal_subscription_builder.js
+++ js/paypal_subscription_builder.js
@@ -1,7 +1,7 @@
 var PayPalSubscriptionBuilder = (function () {
 
-  function toggleIndefinitePayments() {
-    var numberInput = document.getElementById('inputPaymentNumber');
+  function toggleUnlimitedRecurrence() {
+    var numberInput = document.getElementById('inputPaymentCount');
     if (this.checked == true) {
       M.classAdd(numberInput, 'disabled');
     } else {
@@ -9,11 +9,42 @@ var PayPalSubscriptionBuilder = (function () {
     }
   }
 
+  function updatePaymentValue() {
+    var value = parseInt(this.value, 10);
+    document.getElementById('displayPaymentValue').innerHTML = value;
+  }
+
+  function updatePeriodCount() {
+    var value = parseInt(this.value, 10);
+    document.getElementById('displayPeriodCount').innerHTML = value;
+  }
+
+  function updatePaymentCount() {
+    var value = parseInt(this.value, 10);
+    document.getElementById('displayPaymentCount').innerHTML = value;
+  }
+
+  function setUpdater(inputElement, updateFunction) {
+    [ 'change', 'keydown', 'keyup', 'click', 'mousedown' ].forEach(
+      function (eventName) {
+        inputElement.addEventListener(eventName, updateFunction);
+      }
+    );
+    updateFunction.apply(inputElement, updateFunction);
+  }
+
   function load() {
     var button;
-    // Enable indefinite-payment selector.
-    button = document.getElementById('paymentRecursIndefinitely');
-    button.onclick = toggleIndefinitePayments;
+    // Enable numerical inputs.
+    setUpdater(document.getElementById('inputPaymentValue'),
+        updatePaymentValue);
+    setUpdater(document.getElementById('inputPeriodCount'),
+        updatePeriodCount);
+    setUpdater(document.getElementById('inputPaymentCount'),
+        updatePaymentCount);
+    // Enable unlimited-recurrence toggle.
+    button = document.getElementById('unlimitedRecurrence');
+    button.onclick = toggleUnlimitedRecurrence;
   }
 
   return {
